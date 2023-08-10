9b5a198d263d53e4bc10759dc4ad5712685ed94f
mark garcia <markkusag@outlook.com>
2023-08-10 12:20:49 -0500

    08 complete

--- problems/08-value-pair.js
+++ problems/08-value-pair.js
@@ -12,7 +12,27 @@ valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 
 function valuePair(obj1, obj2, key) {
   // Your code here
+  let arr1 = []
+  let obj3 = { ...obj1, ...obj2 }
+  for (let keyy in obj3) {
+
+    if (keyy === key) {
+      if (obj1[keyy] !== undefined) { arr1.push(obj1[keyy]) }
+      if (obj2[keyy] !== undefined) { arr1.push(obj2[keyy]) }
+
+
+    }
+
+
+  }
+  return arr1
+
 }
+
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
+valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuePair;
