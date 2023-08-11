7fc011013c3bc320c07ebf010edd6c97e0d9ccee
Maurice <curtis.virges@gmail.com>
2023-08-11 01:16:23 -0400

    used for of on 06

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -21,16 +21,16 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // Your code here
-  for (let i = 0; i < objArray.length; i++) {
-    let obj = objArray[i];
-    if (keyString in obj) { return true }
-  } return false
+  // for (let i = 0; i < objArray.length; i++) {
+  //   let obj = objArray[i];
+  //   if (keyString in obj) { return true }
+  // } return false
 
-  /*for (let key of objArray) {
-  console.log(objArray[key])
-  if( key === keyString) return true
-  else return false
-}*/
+  for (let obj of objArray) {
+  console.log(obj)
+  if(keyString in obj) return true
+}
+return false
 }
 let objArray = [
   { name: "Rupert" },
@@ -38,9 +38,9 @@ let objArray = [
   { planet: "Earth", system: "Milky Way" }
 ];
 
-//keyInObjectArray(objArray, 'planet'); // => true
-//keyInObjectArray(objArray, 'age'); // => true
-keyInObjectArray(objArray, 'food'); // => false
-//keyInObjectArray(objArray, 'animal'); // => false
+console.log(keyInObjectArray(objArray, 'planet')); // => true
+console.log(keyInObjectArray(objArray, 'age')); // => true
+console.log(keyInObjectArray(objArray, 'food')); // => false
+console.log(keyInObjectArray(objArray, 'animal')); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
